04e1ca5cffa730e6a775041f847ea09bc99703ff
zqm64 <zqm64@outlook.com>
2020-10-28 08:43:35 +0800

    santoni: Import vendor blobs from old santoni dt
    libhwdap.so/Camera firmwares/Graphics firmwares are dropped
    
    8776a27 santoni: biometrics: use prebuilt modified blobs
    197545b santoni: Pin modified Goodix blobs
    34991b6 santoni: rollback fp hals
    5103922 santoni: Build libmm-omxcore
    2f5c39d santoni: kang all Camera blobs from MIUI
    652eaab santoni: fix proprietary files blob list
    bf8f925 santoni: drop unused ACDB blobs
    4723834 santoni: add more acdbdata blobs from MIUI V10.1.1.0 NAMMIFI
    97e2d5d santoni: Move some blobs to /vendor
    65245f8 santoni: move camera to /vendor
    4477746 santoni:move fp to /vendor
    5f63b0f santoni: move firmware to /vendor
    6ec8cd4 santoni: update proprietary-files
    4a7319d santoni: Squash update blobs
    17033f7 santoni: Update Dirac audio from sagit
    9da28f2 santoni: Add Audio ACDB from Stock * Add more audio ACDB blobs from stock to fix Volume adjustment sound
    fe52d84 santoni: Update device-specific blobs list
    aebda27 santoni: Kang tissot wcnss_service
    406954a santoni: Import OSS Camera HAL from CAF
    1afe9de santoni: Add missing props/blobs for fp
    6d396db santoni: Add seperate list for common qcom blobs [1/2]
    3f5f7ea santoni: Add seperate list for common qcom blobs
    947ec02 Revert "Add multicolor LED with prebuilt kernel"
    f1cae22 santoni: drop prebuilt kernel && WLAN driver && set selinux as permissive
    e8b5273 Add multicolor LED with prebuilt kernel
    0159787 Derp
    b8120a3 santoni : Updates Blobs
    be8bcb6 santoni : Updates & Fixes
    fee1de5 santoni : Updating Blobs
    cd20131 santoni: Update Blobs
    cacb8e8 Updates
    e37684b Update proprietary-files.txt

--- extract-files.sh
+++ extract-files.sh
@@ -20,4 +20,8 @@ export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2017
 
+# Hax for cam configs
+CAMERA2_SENSOR_MODULES="${LINEAGE_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
+sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${CAMERA2_SENSOR_MODULES}"
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
