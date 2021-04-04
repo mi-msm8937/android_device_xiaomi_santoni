216ddb2207be1c01488d91fbf49118c76ca97c86
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-04-04 23:52:54 +0800

    santoni: biometrics: Import FP HIDL from lineage-18.1
    Meanwhile also enable fingerprint support.
    
    HEAD is at 297cf8975e647eea7a6b75d5ee0ecd5e70880037

--- device.mk
+++ device.mk
@@ -14,10 +14,18 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku1.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_santoni
 
 # Input
 PRODUCT_COPY_FILES += \
