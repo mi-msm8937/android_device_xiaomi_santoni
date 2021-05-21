efa403fbb0e60451b9b62f65b31b338bb1cdd32f
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-21 09:48:29 +0800

    santoni: biometrics: sepolicy: Import from msm8937-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ VENDOR_SECURITY_PATCH := 2018-10-01
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/biometrics/sepolicy
 
 # Inherit from the proprietary version
 include vendor/xiaomi/santoni/BoardConfigVendor.mk
